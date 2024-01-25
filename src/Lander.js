85142d3d08e1fececd1c09d2656eacf635e71483
Pratick Chakraborty <pratickchakraborty96@gmail.com>
2024-01-25 20:25:28 +0530

    Basic Structure

---
+++ src/Lander.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Cards from "./UIcomponents/Cards";
+import Container from "react-bootstrap/Container";
+export default function Lander() {
+  return (
+    <Container>
+      <Cards />
+    </Container>
+  );
+}
