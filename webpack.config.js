de13828cdac64326266da3a48f0c97ff46ca7a20
Pratick Chakraborty <pratickchakraborty96@gmail.com>
2024-02-27 20:06:42 +0530

    web pack add

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'), // Specify the directory to serve static files from
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+    open: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
